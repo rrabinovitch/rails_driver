9d6bdd78fcea5294127004cd938242371e116063
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-29 15:37:17 -0600

    Set up rails driver

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,5 @@ ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "citext"
 
 end
